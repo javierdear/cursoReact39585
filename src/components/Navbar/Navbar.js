01058f82baf9e7f3ee7be55f1c0976c8a1872478
javier de arce <dearcejavier@gmail.com>
2023-02-22 23:53:48 -0300

    adding logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return(
         <header className="header">
             <div className="header__container">
-                <h1 className="header__logo">LOGO</h1>
+                <h1 className="header__logo">La Unión del Mate</h1>
         
                 <nav className="navbar">
                     <a href="#" className="navbar__link">Inicio</a>
